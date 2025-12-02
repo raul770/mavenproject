1924916a1ad69848c3606463b6982294eda2ee1d
raul770 <ragomez@itpfp.com>
2025-12-02 14:52:57 +0100

    Subir proyecto

---
+++ src/main/java/com/mycompany/githubejercicio/Githubejercicio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.githubejercicio;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Githubejercicio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
